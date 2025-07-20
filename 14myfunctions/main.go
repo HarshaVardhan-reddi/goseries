f6c2594440882c23e93f87bea060649f7b46367b
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-07-20 09:35:39 +0530

    functions in golang

--- 14myfunctions/main.go
+++ 14myfunctions/main.go
@@ -2,8 +2,45 @@ package main
 
 import "fmt"
 
-// adding comment for testing git user profile issue
+// func methodname(*params) returntype {}, if void noting to mention example greeter function
+func addTwoNumbers(a int, b int)int {
+	var sum int = a+b
+	return sum
+}
 
+// Entry point function to the program
+// Anonymous functions exists in go
 func main(){
 	fmt.Println("Welcome to functions in go")
+	greeter()
+	result := addTwoNumbers(1,2)
+	fmt.Println("output of the two numbers:",result)
+	fmt.Println("Adding all the numbers passed to the function", proAdder(1,2,4,5,6))
+
+	sum, message := multiReturns(1,2,3,4,4,4,4,10) // storing multivalued result
+	fmt.Println("Message which we got: ", message, "\nand the sum is: ", sum)
+}
+
+func greeter(){
+	fmt.Println("Namaste from golang")
+}
+
+// Pro functions
+func proAdder(values ...int)int {
+	sum := 0
+	for _, element := range values {
+		sum += element
+	}
+	return sum
+}
+
+
+// Pro functions
+// Two values can be returned from a function
+func multiReturns(values ...int)(int,string) {
+	sum := 0
+	for _, element := range values {
+		sum += element
+	}
+	return sum, "Hello this is a string"
 }

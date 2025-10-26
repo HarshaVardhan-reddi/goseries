b95cd834e976b6752208ab8c86ca68c270ff7432
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-10-26 17:23:12 +0530

    change package name to encoder

--- 20myjsonhanlding/encoder/encoder.go
+++ 20myjsonhanlding/encoder/encoder.go
@@ -1,4 +1,4 @@
-package decoders
+package encoders
 
 import (
 	"encoding/json"
--- 20myjsonhanlding/main.go
+++ 20myjsonhanlding/main.go
@@ -2,7 +2,7 @@ package main
 
 import(
 	"fmt"
-	encoders "myjsonhanlding/encoder"
+	"myjsonhanlding/encoder"
 )
 
 type course struct{

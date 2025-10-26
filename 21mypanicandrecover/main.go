40ebe69226367f00a74b9b070b645e563d81cfa2
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-10-26 18:27:15 +0530

    panic and recover in golang

---
+++ 21mypanicandrecover/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Welcome to pnaic tutorial")
+
+	defer func ()  {
+		r := recover()
+	
+		if(r != nil){
+			// caching the error
+			fmt.Println(r)
+		}
+	}()
+
+	panic("Raising panic here!!!")
+}

6f933a5e2168ab2f9bce9a48e317f96af5097842
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-10-26 12:25:05 +0530

    accessing json data with backticks

--- 19postverb/main.go
+++ 19postverb/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strings"
 )
+
+type DataStrct struct {
+	Year int
+	Price float32
+	CPUModel string `json:"CPU model"`
+	HardDiskSize string `json:"Hard disk size"`
+}
+
+type Info struct{
+	Id string
+	Name string
+	Data DataStrct
+}
 
 func main(){
 	fmt.Println("sending a post request in golang")
@@ -34,5 +48,14 @@ func PerformPostRequest(){
 
 	content, _ := io.ReadAll(response.Body)
 	fmt.Println(string(content))
+
+	info := Info{}
+
+	errParse := json.Unmarshal(content, &info)
+	if errParse != nil {
+		panic(errParse)
+	}
+
+	fmt.Println(info)
 	
 }

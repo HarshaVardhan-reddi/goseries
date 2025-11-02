02399e140636cea33ee06f0176e1dcf1d8a26bf6
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-11-02 11:30:51 +0530

    unmarshelling json using interface

--- 20myjsonhanlding/decoders/decoder.go
+++ 20myjsonhanlding/decoders/decoder.go
@@ -13,6 +13,15 @@ func ParseInputStringifiedJson(input string) encoders.DataForEncode {
 	fmt.Println(bytedinput)
 
 	data := encoders.DataForEncode{}
+	validJson := json.Valid(bytedinput)
+	if validJson {
+		fmt.Println("Processing json as it is valid")
+		response := map[string]interface{}{} // all the data types implemenmts an empty interface by default
+		json.Unmarshal(bytedinput, &response)
+		fmt.Println(response)
+	} else {
+		panic("Invalid json")
+	}
 
 	err := json.Unmarshal(bytedinput, &data)
 	if err != nil {
@@ -22,3 +31,7 @@ func ParseInputStringifiedJson(input string) encoders.DataForEncode {
 	// fmt.Println(data)
 	return data
 }
+
+// So in prod grade systems
+// response := map[string]interface{}{} this will be used initially as a raw repsonse
+// then the raw response will be validated and mapped to a defined struct

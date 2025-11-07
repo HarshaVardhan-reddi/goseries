d0cf166c38c15d09b1a18725ba8ceea46c60e2f0
HarshaVardhan-reddi <harshapreddy9@gmail.com>
2025-11-07 19:18:44 +0530

    yml file parser in golang

--- misc/main.go
+++ misc/main.go
@@ -5,4 +5,5 @@ import workingwithyaml "misc/working_with_yaml"
 func main() {
 	println("Welcome to the misc programs in golang") // everytime not required to include fmt
 	workingwithyaml.DecodeSampleYaml()
+	workingwithyaml.DecodeYamlFile()
 }
--- misc/working_with_yaml/yamldecoder.go
+++ misc/working_with_yaml/yamldecoder.go
@@ -2,13 +2,26 @@ package workingwithyaml
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"strings"
+
 	"github.com/goccy/go-yaml"
 )
 
 type SampleYaml struct{
 	A int `yaml:"a"`
 	B int `yaml:"b"`
+}
+
+type ExampleYaml struct{
+	A int `yaml:"a"`
+	B int `yaml:"b"`
+	C int `yaml:"c"`
+}
+
+type ExampleParent struct{
+	Numbers ExampleYaml `yaml:"numbers"`
 }
 
 func DecodeSampleYaml() {
@@ -26,4 +39,21 @@ b: 2
 		panic(err)
 	}
 	fmt.Println("Unmarshalled YAML", sample)
+}
+
+
+func DecodeYamlFile(){
+	file, err := os.OpenFile("/Users/harshavardhanreddy/goseries/misc/working_with_yaml/example.yml",0,os.FileMode(os.O_RDONLY))
+	if err != nil{
+		panic(err)
+	}
+	bytedoutput, errread := io.ReadAll(file)
+	if errread != nil{
+		panic(errread)
+	}
+	examplefile := ExampleParent{}
+	if err := yaml.Unmarshal(bytedoutput, &examplefile); err != nil{
+		panic(err)
+	}
+	fmt.Printf("examplefile: %v\n", examplefile)
 }
